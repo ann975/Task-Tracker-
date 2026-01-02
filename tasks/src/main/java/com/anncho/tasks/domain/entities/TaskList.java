aadf6c5e017167943875ff6cd0cb57798fbd1c37
ann975 <annsangeuncho@gmail.com>
2026-01-01 19:54:19 -0500

    Task and TaskList edits

--- tasks/src/main/java/com/anncho/tasks/domain/entities/TaskList.java
+++ tasks/src/main/java/com/anncho/tasks/domain/entities/TaskList.java
@@ -1,19 +1,17 @@
 package com.anncho.tasks.domain.entities;
 
-import java.util.List;
-
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 
 @Entity
@@ -34,7 +32,13 @@ public class TaskList {
     // establish relationship between task list and tasks
     // one task list to many tasks 
     // mappedBy - look to instance variable taskList in Task class for more configuration
-    @OneToMany(mappedBy = "taskList", cascade= {CascadeType.REMOVE})
+    //cascadetype.remove - when deleting tasklist all tasks will be deleted too
+    //cascadetype.persist - when we save list any new task it contains will be saved too
+    @OneToMany(mappedBy = "taskList", cascade= {
+        CascadeType.REMOVE,
+        CascadeType.PERSIST
+
+    })
     private List<Task> tasks;
 
     @Column(name="created", nullable=false)
